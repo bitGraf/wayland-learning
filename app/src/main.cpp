11f5566cba2ee12cd452cd7310cb511fcd356a65
bitgraf <ablaskov27@gmail.com>
2026-09-14 13:19:10 -0500

    5.1 Binding to globals

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -5,6 +5,13 @@
 // wayland headers
 #include <wayland-client.h>
 
+static void registry_handle_global(void* data, wl_registry* registry, uint32_t name, const char* interface, uint32_t version) {
+	printf("interface: '%s', version: %d, name: %d\n", interface, version, name);
+}
+static void registry_handle_global_remove(void* data, wl_registry* registry, uint32_t name) {
+	// do nothing
+}
+
 int main(int argc, char* argv[]) {
 	std::cout << "Creating wl_display..." << std::endl;
 
@@ -13,7 +20,23 @@ int main(int argc, char* argv[]) {
 		fprintf(stderr, "Failed to connect to Wayland display.\n");
 		return 1;
 	}
-	fprintf(stderr, "Connection established!\n");
+	fprintf(stdout, "Connection established!\n");
+
+	wl_registry* registry = wl_display_get_registry(display);
+	if (!registry) {
+		fprintf(stderr, "Filed to get registry from display.\n");
+		return 1;
+	}
+	fprintf(stdout, "Registry retrieved!\n");
+
+	wl_registry_listener registry_listener;
+	registry_listener.global = registry_handle_global;
+	registry_listener.global_remove = registry_handle_global_remove;
+	wl_registry_add_listener(registry, &registry_listener, NULL);
+	fprintf(stdout, "registry listener attached.\n");
+	fprintf(stdout, "%d events occured.\n", wl_display_roundtrip(display));
+	fprintf(stdout, "%d events occured.\n", wl_display_roundtrip(display));
+	
 
 	#if 0
 	long ms_elapsed = 0;
