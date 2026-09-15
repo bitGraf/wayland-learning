f1876435f04a8fef1298cf689a7119caaac2128a
bitgraf <ablaskov27@gmail.com>
2026-09-15 14:59:28 -0500

    some basic vulkan setup

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -4,10 +4,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <vector>
 
 // vulkan headers
 #include <vulkan/vulkan.h>
 #include <vulkan/vulkan_wayland.h>
+
+// VulkanMemoryAllocator
+#define VMA_IMPLEMENTATION // define this in ONE source file...
+#include <vk_mem_alloc.h>
 
 // wayland headers
 #include <wayland-client.h>
@@ -18,6 +23,12 @@
 #define CHECK_WL_RESULT(_expr) \
 if (!(_expr)) { \
     printf("Error executing %s.\n", #_expr);\
+}
+
+#define CHECK_VK_RESULT(_expr) \
+result = _expr; \
+if (result != VK_SUCCESS) { \
+	printf("Error executing %s: %i\n", #_expr, result); \
 }
 
 // global state variable
@@ -32,6 +43,15 @@ struct client_state {
 	xdg_wm_base* shell;
 	xdg_surface* shell_surface;
 	xdg_toplevel* shell_toplevel;
+
+	// Vulkan
+	VkInstance instance;
+	VkPhysicalDevice phys_device;
+	VkDevice device;
+	VkQueue queue;
+	VkSwapchainKHR swapchain;
+	VkSurfaceKHR vulkan_surface;
+	VmaAllocator allocator;
 
 	// program control
 	bool done = false;
@@ -84,6 +104,7 @@ static const xdg_toplevel_listener _xdg_toplevel_listener = {
 int main(int argc, char* argv[]) {
 	client_state state = {};
 
+	printf("========================Wayland Setup=================================\n");
 	CHECK_WL_RESULT(state.display = wl_display_connect(NULL));
 
 	CHECK_WL_RESULT(state.registry = wl_display_get_registry(state.display));
@@ -114,9 +135,151 @@ int main(int argc, char* argv[]) {
 	printf("   xdg_surface =  %p\n", (void*)state.shell_surface);
 	printf("   xdg_toplevel = %p\n", (void*)state.shell_toplevel);
 	printf("}\n");
+	printf("======================================================================\n\n\n");
 
-	printf("Start main event loop...\n\n");
+	// setup Vulkan 
+	printf("========================Vulkan Setup==================================\n");
+	{
+		VkResult result;
 
+		VkApplicationInfo appInfo {
+			.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+			.pApplicationName = "How to Vulkan",
+			.apiVersion = VK_API_VERSION_1_3
+		};
+
+		const char* const instanceExtensionNames[] = {
+			"VK_EXT_debug_utils",
+			"VK_KHR_surface",
+			"VK_KHR_wayland_surface"
+		};
+		VkInstanceCreateInfo createInfo {
+			.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+			.pApplicationInfo = &appInfo,
+			.enabledExtensionCount = sizeof(instanceExtensionNames) / sizeof(const char*),
+			.ppEnabledExtensionNames = instanceExtensionNames
+		};
+		CHECK_VK_RESULT(vkCreateInstance(&createInfo, nullptr, &state.instance));
+
+		uint32_t deviceCount = 0;
+		CHECK_VK_RESULT(vkEnumeratePhysicalDevices(state.instance, &deviceCount, nullptr));
+		std::vector<VkPhysicalDevice> devices(deviceCount);
+		CHECK_VK_RESULT(vkEnumeratePhysicalDevices(state.instance, &deviceCount, devices.data()));
+
+		uint32_t num_devices = (uint32_t)devices.size();
+		printf("%d devices found:\n", num_devices);
+		for (uint32_t n = 0; n < num_devices; n++) {
+			VkPhysicalDeviceProperties2 deviceProperties { .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PROPERTIES_2 };
+			vkGetPhysicalDeviceProperties2(devices[n], &deviceProperties);
+			printf("  Selected device: %s\n", deviceProperties.properties.deviceName);
+		}
+		// manually choose the first physical device
+		state.phys_device = devices[0];
+
+		// find graphics capable queue
+		uint32_t queueFamilyCount = 0;
+		vkGetPhysicalDeviceQueueFamilyProperties(state.phys_device, &queueFamilyCount, nullptr);
+		std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+		vkGetPhysicalDeviceQueueFamilyProperties(state.phys_device, &queueFamilyCount, queueFamilies.data());
+		uint32_t queueFamily = 0;
+		for (size_t n = 0; n < queueFamilies.size(); n++) {
+			if (queueFamilies[n].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+				queueFamily = (uint32_t)n;
+				break;
+			}
+		}
+
+		const float qfpriorities = 1.0f;
+		VkDeviceQueueCreateInfo queueInfo {
+			.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+			.queueFamilyIndex = queueFamily,
+			.queueCount = 1,
+			.pQueuePriorities = &qfpriorities,
+		};
+
+		VkPhysicalDeviceVulkan12Features enabledVk12Features{
+			.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES,
+			.descriptorIndexing = true,
+			.shaderSampledImageArrayNonUniformIndexing = true,
+			.descriptorBindingVariableDescriptorCount = true,
+			.runtimeDescriptorArray = true,
+			.bufferDeviceAddress = true
+		};
+		VkPhysicalDeviceVulkan13Features enabledVk13Features {
+			.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES,
+			.pNext = &enabledVk12Features,
+			.synchronization2 = true,
+			.dynamicRendering = true
+		};
+		VkPhysicalDeviceFeatures enabledVk10Features {
+			.samplerAnisotropy = VK_TRUE
+		};
+
+		const std::vector<const char*> deviceExtensions { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
+		VkDeviceCreateInfo deviceCreateInfo {
+			.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+			.pNext = &enabledVk13Features,
+			.queueCreateInfoCount = 1,
+			.pQueueCreateInfos = &queueInfo,
+			.enabledExtensionCount = static_cast<uint32_t>(deviceExtensions.size()),
+			.ppEnabledExtensionNames = deviceExtensions.data(),
+			.pEnabledFeatures = &enabledVk10Features
+		};
+		CHECK_VK_RESULT(vkCreateDevice(state.phys_device, &deviceCreateInfo, nullptr, &state.device));
+
+		vkGetDeviceQueue(state.device, queueFamily, 0, &state.queue);
+
+		// setup VMA
+		VmaVulkanFunctions vkFunctions {
+			.vkGetInstanceProcAddr = vkGetInstanceProcAddr,
+			.vkGetDeviceProcAddr = vkGetDeviceProcAddr,
+			.vkCreateImage = vkCreateImage
+		};
+		VmaAllocatorCreateInfo allocatorCreateInfo {
+			.flags = VMA_ALLOCATOR_CREATE_BUFFER_DEVICE_ADDRESS_BIT,
+			.physicalDevice = state.phys_device,
+			.device = state.device,
+			.pVulkanFunctions = &vkFunctions,
+			.instance = state.instance
+		};
+		CHECK_VK_RESULT(vmaCreateAllocator(&allocatorCreateInfo, &state.allocator));
+	}
+	// create wayland/vulkan surface
+	{
+		VkResult result;
+
+		VkWaylandSurfaceCreateInfoKHR createInfo {
+			.sType = VK_STRUCTURE_TYPE_WAYLAND_SURFACE_CREATE_INFO_KHR,
+			.display = state.display,
+			.surface = state.surface
+		};
+		CHECK_VK_RESULT(vkCreateWaylandSurfaceKHR(state.instance, &createInfo, NULL, &state.vulkan_surface));
+
+		VkSurfaceCapabilitiesKHR surfaceCaps;
+		CHECK_VK_RESULT(vkGetPhysicalDeviceSurfaceCapabilitiesKHR(state.phys_device, state.vulkan_surface, &surfaceCaps));
+
+		const VkFormat imageFormat = VK_FORMAT_B8G8R8A8_SRGB;
+		VkSwapchainCreateInfoKHR swapchainCreateInfo {
+			.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR,
+			.surface = state.vulkan_surface,
+			.minImageCount = surfaceCaps.minImageCount,
+			.imageFormat = imageFormat,
+			.imageColorSpace = VK_COLORSPACE_SRGB_NONLINEAR_KHR,
+			.imageExtent { .width = 900, .height = 600 },
+			.imageArrayLayers = 1,
+			.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
+			.preTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR,
+			.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR,
+			.presentMode = VK_PRESENT_MODE_FIFO_KHR
+		};
+		CHECK_VK_RESULT(vkCreateSwapchainKHR(state.device, &swapchainCreateInfo, nullptr, &state.swapchain));
+	}
+
+	printf("======================================================================\n\n\n");
+
+	// start main loop
+	printf("====================Start main event loop...==========================\n");
+	printf("  running...\n");
 	int count = 0;
 	while (!state.done) {
 		sleep_ms(100);
@@ -124,8 +287,10 @@ int main(int argc, char* argv[]) {
 		wl_display_roundtrip(state.display);
 
 		count++;
-		if (count > 20) break;
+		if (count > 5) break;
 	}
+	printf("  done.\n");
+	printf("======================================================================\n\n\n");
 
 	// Cleanup
 	printf("Cleaning up resources.\n");
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
 
-export WAYLAND_DEBUG=1
+export WAYLAND_DEBUG=0
 
 ./bin/app
